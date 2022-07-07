73c70ed59fc6f4cf8253df7ea8400a8f8b708133
Cortez <j.cortez.jmc@gmail.com>
2022-07-07 03:48:25 -0700

    7/7/2022

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router } from "react-router-dom";
+import { Navbar } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+        <nav className="App-nav">
+          <navbar className="App-navbar">
+
+          </navbar>
+        </nav>
+
     </div>
   );
 }
