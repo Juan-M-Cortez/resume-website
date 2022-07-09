7e7f3fc45b47844620332b4b2d8a6ac912736bef
Cortez <j.cortez.jmc@gmail.com>
2022-07-09 14:37:43 -0700

    7/9/2022

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,9 +6,9 @@ export default function Navbar() {
         <nav className="navbar-nav">
             <div className='navbar-container'>
                 <ul className='navbar-ul'>
-                    <li className='navbar-li'><a>Home</a></li>
-                    <li className='navbar-li'><a>Resume</a></li>
-                    <li className='navbar-li'><a>Projects</a></li>
+                    <li className='navbar-li'><a className='nav-links'>Home</a></li>
+                    <li className='navbar-li'><a className='nav-links'>Resume</a></li>
+                    <li className='navbar-li'><a className='nav-links'>Projects</a></li>
                 </ul>
                 <ul className='navbar-icon-container'>
                     <li><a className='icon-anchor'><i className='linkin'/></a></li>
