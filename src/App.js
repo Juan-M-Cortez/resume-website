fd46c5e972acf57287e5d612936f6ba00f82c578
Cortez <j.cortez.jmc@gmail.com>
2022-07-09 16:16:17 -0700

    Footer&Split

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar style={{ zindex: 3 }}/>
-        
+        <Navbar style={{ zindex: 3 }} />
+
         <Routes>
           <Route path="/"
-          element={
-            <Home style={{ zindex: 0 }}/>
-          }/>
+            element={
+              <Home style={{ zindex: 0 }} />
+            } />
         </Routes>
+
+        <Footer />
 
       </div>
     </Router>
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <nav className="footer-nav">
+            <div className='navbar-container'>
+                <ul className='navbar-ul'>
+                    <li className='navbar-li'><a className='nav-links'>Home</a></li>
+                    <li className='navbar-li'><a className='nav-links'>Resume</a></li>
+                    <li className='navbar-li'><a className='nav-links'>Projects</a></li>
+                </ul>
+                <ul className='navbar-icon-container'>
+                    <li><a className='icon-anchor'><i className='linkin' /></a></li>
+                    <li><a className='icon-anchor'><i className='github' /></a></li>
+                    <li><a className='icon-anchor'><i className='instagram' /></a></li>
+                </ul>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Footer
