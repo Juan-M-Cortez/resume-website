3dfca484582a12a03c70ad80906bc2e9f104539d
Cortez <j.cortez.jmc@gmail.com>
2022-07-08 23:22:42 -0700

    7/8/2022

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home-container'>
+    <div className='profile'>
+        Profile
+    </div>
+    <div className='profile-details'>
+        Details
+    </div>
+    </div>
   )
 }
 
