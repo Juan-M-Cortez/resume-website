fc16887763be1e68cef6221239181820d477c715
Cortez <j.cortez.jmc@gmail.com>
2022-07-12 13:45:55 -0700

    tittle & content

--- src/components/home/details/Details.js
+++ src/components/home/details/Details.js
@@ -5,16 +5,17 @@ import React from 'react';
 import './Details.css';
 
 function Details() {
+
     return (
         <div className='details'>
             <Banner />
             <div className='card-container'>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Card tittle={'Web Development'} content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus esse commodi deserunt vitae, vero quasi! Veniam quaerat tenetur pariatur doloribus.'}/>
+                <Card tittle={'UI/UX Design'} content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus esse commodi deserunt vitae, vero quasi! Veniam quaerat tenetur pariatur doloribus.'}/>
+                <Card tittle={'Sound Design'} content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus esse commodi deserunt vitae, vero quasi! Veniam quaerat tenetur pariatur doloribus.'}/>
+                <Card tittle={'Game Design'} content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus esse commodi deserunt vitae, vero quasi! Veniam quaerat tenetur pariatur doloribus.'}/>
+                <Card tittle={'2D/3D Design'} content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus esse commodi deserunt vitae, vero quasi! Veniam quaerat tenetur pariatur doloribus.'}/>
+                <Card tittle={'GraphicArts'} content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus esse commodi deserunt vitae, vero quasi! Veniam quaerat tenetur pariatur doloribus.'}/>
             </div>
             <CardSwipe />
         </div>
--- src/components/home/details/card/Card.js
+++ src/components/home/details/card/Card.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function Card() {
+function Card(props) {
+  const {tittle, content} = props;
   return (
     <div className='card'>
-        
+        <h5 className='card-tittle'>{tittle}</h5>
+        <div className='card-content'>{content}</div>
     </div>
   )
 }
