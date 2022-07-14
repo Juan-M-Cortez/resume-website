527eb2006f8cf0ca24eb10762095758771bdae54
Cortez <j.cortez.jmc@gmail.com>
2022-07-14 04:19:53 -0700

    profile edits

--- src/components/home/profile/Part4.js
+++ src/components/home/profile/Part4.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Part4() {
   return (
-    <div>
-        Part4
+    <div className='part4'>
+        <div className='part4-divider'/>
+        <p className='part4-p'>part4</p>
     </div>
   )
 }
