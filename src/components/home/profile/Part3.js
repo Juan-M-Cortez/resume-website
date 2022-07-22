2d187e8f063e2a94cfeb1ad4be8cbd3794989627
Cortez <j.cortez.jmc@gmail.com>
2022-07-22 14:19:17 -0700

    language path fix

--- src/components/home/profile/Part3.js
+++ src/components/home/profile/Part3.js
@@ -31,9 +31,10 @@ function Part3() {
                 <div className='svg-container'>
                     <div className='part3-svg-size'>
                         <svg className='part3-svg'>
-                            <path d="m 85 50 a 8 8 0 0 0 -70 0 a 8 8 0 0 0 70 -1" className='part3-path' />
+                            <path d="m 85 50 a 8 8 0 0 0 -70 0 a 8 8 0 0 0 70 -1" className='negative-path'/>
+                            <path d="m 85 50 a 8 8 0 0 0 -70 0 a 8 8 0 0 0 70 -1" className='part3-path path3' />
                         </svg>
-                        <div className='part3-p'>100%</div>
+                        <div className='part3-p'>25%</div>
                     </div>
                     <p className='part3-lp'>French</p>
                 </div>
