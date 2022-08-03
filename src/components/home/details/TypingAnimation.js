0abd8faf8168719b15f5f1b4268a99963ce3dd17
Cortez <j.cortez.jmc@gmail.com>
2022-08-03 14:02:36 -0700

    typing animation

---
+++ src/components/home/details/TypingAnimation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TypingAnimation.css'
+
+function TypingAnimation() {
+    return (
+        <div class="typewriter">
+            <h1>Welcome To My Website.</h1>
+        </div>
+    )
+}
+
+export default TypingAnimation
--- src/components/home/details/banner/Banner.js
+++ src/components/home/details/banner/Banner.js
@@ -1,9 +1,13 @@
 import BannerPic from '../../../../pictures/face-2-min.png';
+import TypingAnimation from '../TypingAnimation';
 import React from 'react'
 
 function Banner() {
   return (
     <div className='banner'>
+      <div className='banner-animation-container'>
+        <TypingAnimation />
+      </div>
       {/*
       <div className='banner-div-span'>
         <span className='banner-span'>EXPLORE NOW</span>
